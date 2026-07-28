0a18781bc1d443f2a619e04b589125c42bbee341
David <davidi.kjellberg@gmail.com>
2026-07-29 00:25:54 +0200

    small version fix and updater check

--- src/main/java/org/net/AppVersion.java
+++ src/main/java/org/net/AppVersion.java
@@ -1,7 +1,7 @@
 package org.net;
 
 public final class AppVersion {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     private AppVersion() {}
 }
