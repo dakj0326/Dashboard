63d53d2201084950d5162bb30b98c13bc956751d
dakj0326 <81382353+dakj0326@users.noreply.github.com>
2026-07-29 00:52:06 +0200

    Update AppVersion to version 1.0.2

--- src/main/java/org/net/AppVersion.java
+++ src/main/java/org/net/AppVersion.java
@@ -1,7 +1,7 @@
 package org.net;
 
 public final class AppVersion {
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     private AppVersion() {}
 }
